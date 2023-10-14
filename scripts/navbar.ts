f70b8ce456a4a267860bc7bc6cad20708aa10bb6
TZU-CHAO WANG <john29917958@gmail.com>
2023-10-14 20:43:09 +0800

    Add expand and collapse feature to navbar

--- scripts/navbar.ts
+++ scripts/navbar.ts
@@ -1,8 +1,20 @@
 const navbar = {
-  init: function (navbarElem: any): void {},
-
-  testShowMessage: function (message: string): void {
-    console.log(message);
+  init: function (navbar: Element): void {
+    const navToggler = navbar.querySelector(".nav-toggler");
+    navToggler?.addEventListener("click", () => {
+      const isToggled = navbar.classList.contains("toggled");
+      if (isToggled) {
+        navbar.classList.remove("toggled");
+      } else {
+        navbar.classList.add("toggled");
+      }
+    });
+    const navCover = document.createElement("div");
+    navCover.classList.add("nav-cover");
+    navCover.addEventListener("click", () => {
+      navbar.classList.remove("toggled");
+    });
+    navbar.appendChild(navCover);
   },
 };
 
