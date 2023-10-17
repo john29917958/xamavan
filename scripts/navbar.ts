1ee2e5ac1b47f03297159303e19fa254dec90842
TZU-CHAO WANG <john29917958@gmail.com>
2023-10-17 19:19:46 +0800

    Add dropdown feature to navigation bar

--- scripts/navbar.ts
+++ scripts/navbar.ts
@@ -1,21 +1,58 @@
 const navbar = {
   init: function (navbar: Element): void {
-    const navToggler = navbar.querySelector(".nav-toggler");
-    navToggler?.addEventListener("click", () => {
-      const isToggled = navbar.classList.contains("toggled");
-      if (isToggled) {
-        navbar.classList.remove("toggled");
-      } else {
-        navbar.classList.add("toggled");
-      }
-    });
-    const navCover = document.createElement("div");
-    navCover.classList.add("nav-cover");
-    navCover.addEventListener("click", () => {
-      navbar.classList.remove("toggled");
-    });
-    navbar.appendChild(navCover);
+    initToggleFeature(navbar);
+    initDropdowns(navbar);
   },
 };
+
+function initToggleFeature(navbar: Element) {
+  const navToggler = navbar.querySelector(".nav-toggler");
+  navToggler?.addEventListener("click", () => {
+    toggleElem(navbar);
+  });
+  const navCover = document.createElement("div");
+  navCover.classList.add("nav-cover");
+  navCover.addEventListener("click", () => {
+    navbar.classList.remove("toggled");
+  });
+  navbar.appendChild(navCover);
+}
+
+function initDropdowns(navbar: Element) {
+  const dropdownItems: NodeListOf<Element> =
+    navbar.querySelectorAll("li.dropdown");
+  for (const dropdownItem of dropdownItems) {
+    dropdownItem.addEventListener("click", function (e) {
+      toggleElem(dropdownItem);
+      const menu: HTMLElement = dropdownItem.querySelector(
+        ".dropdown-menu"
+      ) as HTMLElement;
+      const elemBounds: DOMRect = menu.getBoundingClientRect();
+      const elemBottom: number = elemBounds.bottom;
+      const windowBottom: number = window.innerHeight;
+      if (elemBottom > windowBottom) {
+        menu.style.height =
+          (elemBounds.height - (elemBottom - windowBottom)).toString() + "px";
+      } else {
+        menu.style.height = "auto";
+      }
+      e.stopPropagation();
+      function handleClickOutsideDropdown() {
+        toggleElem(dropdownItem);
+        window.removeEventListener("click", handleClickOutsideDropdown);
+      }
+      window.addEventListener("click", handleClickOutsideDropdown);
+    });
+  }
+}
+
+function toggleElem(elem: Element) {
+  const isToggled = elem.classList.contains("toggled");
+  if (isToggled) {
+    elem.classList.remove("toggled");
+  } else {
+    elem.classList.add("toggled");
+  }
+}
 
 export { navbar };
