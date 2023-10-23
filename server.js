b3087016172f7cef38d28ef0d845a05361a3f9cf
TZU-CHAO WANG <john29917958@gmail.com>
2023-10-23 12:00:33 +0800

    Make server being able to run in prod and dev modes

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const https = require("https");
 const fs = require("fs");
+const process = require("process");
 const app = express();
 
 app.use(express.static("public"));
-
-const options = {
-  key: fs.readFileSync(""),
-  cert: fs.readFileSync(""),
-};
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/" + "index.html");
@@ -18,10 +14,57 @@ app.get("/portfolio/business-web-design-sample", (req, res) => {
   res.sendFile(__dirname + "/public/business-web-design-sample.html");
 });
 
-const server = https.createServer(options, app).listen(443);
+if (process.env.NODE_ENV === "production") {
+  loadSslConfig(
+    process.env["SSL_PRIVKEY_PATH"],
+    process.env["SSL_CERT_PATH"],
+    (sslConfigs) => {
+      const server = https.createServer(sslConfigs, app).listen(443);
+    }
+  );
+} else {
+  const server = app.listen(80, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log("App listening at http://%s:%s", host, port);
+  });
+}
 
-// const server = app.listen(80, () => {
-//   const host = server.address().address;
-//   const port = server.address().port;
-//   console.log("App listening at http://%s:%s", host, port);
-// });
+function loadSslConfig(keyPath, certPath, onComplete) {
+  new Promise((resolve, reject) => {
+    fs.readFile(keyPath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  })
+    .then(
+      (sslKey) => {
+        return new Promise((resolve, reject) => {
+          fs.readFile(certPath, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve({
+                key: sslKey,
+                cert: data,
+              });
+            }
+          });
+        });
+      },
+      (err) => {
+        throw err;
+      }
+    )
+    .then(
+      (sslOptions) => {
+        onComplete(sslOptions);
+      },
+      (err) => {
+        throw err;
+      }
+    );
+}
