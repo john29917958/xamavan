d94bb86c1cceb262415e26594b9b8630ffc4ae34
TZU-CHAO WANG <john29917958@gmail.com>
2023-10-23 09:53:35 +0800

    Add sample SSL listening code

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const app = express();
 
 app.use(express.static("public"));
+
+const options = {
+  key: fs.readFileSync(""),
+  cert: fs.readFileSync(""),
+};
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/" + "index.html");
@@ -11,8 +18,10 @@ app.get("/portfolio/business-web-design-sample", (req, res) => {
   res.sendFile(__dirname + "/public/business-web-design-sample.html");
 });
 
-const server = app.listen(80, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log("App listening at http://%s:%s", host, port);
-});
+const server = https.createServer(options, app).listen(443);
+
+// const server = app.listen(80, () => {
+//   const host = server.address().address;
+//   const port = server.address().port;
+//   console.log("App listening at http://%s:%s", host, port);
+// });
