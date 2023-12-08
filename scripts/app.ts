2cb18eb4a2a9698fd7c76e4c7005e31bb3c644a7
TZU-CHAO WANG <john29917958@gmail.com>
2023-12-08 20:43:48 +0800

    Add on viewport entry animation for section 2 in home page

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,11 +1,12 @@
 import { navbar } from "./navbar";
 import { scrollDownToButton } from "./scrollDownToButton";
+import { transition } from "./transition";
 
 window.addEventListener("load", () => {
   const navbars: NodeListOf<Element> = document.querySelectorAll("nav.nav");
   for (const navbarElem of navbars) {
     navbar.init(navbarElem);
   }
-
   scrollDownToButton.init();
+  transition.init();
 });
