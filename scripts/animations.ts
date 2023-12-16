388a5ff8572afbaf439ec136056a74b9aa7fb7ec
TZU-CHAO WANG <john29917958@gmail.com>
2023-12-16 20:46:20 +0800

    Refactor animation TypeScript code

--- scripts/animations.ts
+++ scripts/animations.ts
@@ -1,20 +1,14 @@
+import { NetworkBackgroundAnimation } from "./animations/networkBackgroundAnimation";
+
 const animations = {
   initNetworkBgAnimation(elems?: Array<HTMLElement> | NodeListOf<Element>) {
     const elemsToInit: Element[] = getElemsToInit(
       ".bg-animation-network",
       elems
     );
-    for (const elem of elemsToInit) {
-      const canvas: HTMLCanvasElement = getBgCanvas();
-      initNetworkBgAnimation(canvas);
-      elem.append(canvas);
-      const elemAsHtml: HTMLElement = elem as HTMLElement;
-      if (elemAsHtml.style.position.length === 0) {
-        // Set parent element position for canvas to be positioned with it.
-        // TODO: Extract the code to a readable function.
-        elemAsHtml.style.position = "relative";
-      }
-    }
+    const networkBackgroundAnimation: NetworkBackgroundAnimation =
+      new NetworkBackgroundAnimation(elemsToInit);
+    networkBackgroundAnimation.init();
   },
 };
 
@@ -31,143 +25,6 @@ function getElemsToInit(
     elemsToInit = Array.from(elems);
   }
   return elemsToInit;
-}
-
-function getBgCanvas(): HTMLCanvasElement {
-  const canvas: HTMLCanvasElement = document.createElement("canvas");
-  canvas.style.position = "absolute";
-  canvas.style.top = "0";
-  canvas.style.left = "0";
-  canvas.style.width = "100%";
-  canvas.style.height = "100%";
-  canvas.style.zIndex = "-1";
-  canvas.width = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight;
-  canvas.classList.add("trans-entrance-fade-in", "trans-d-1500");
-  return canvas;
-}
-
-function initNetworkBgAnimation(canvas: HTMLCanvasElement) {
-  let ctx: CanvasRenderingContext2D = canvas.getContext(
-    "2d"
-  ) as CanvasRenderingContext2D;
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  interface Star {
-    x: number;
-    y: number;
-    radius: number;
-    vx: number;
-    vy: number;
-  }
-  interface Point {
-    x: number;
-    y: number;
-  }
-  let stars: Star[] = [], // Array that contains the stars
-    FPS = 60, // Frames per second
-    x = getNumOfStars(), // Number of stars
-    mouse = {
-      x: 0,
-      y: 0,
-    }; // mouse location
-
-  // Push stars to array
-  for (var i = 0; i < x; i++) {
-    stars.push({
-      x: Math.random() * canvas.width,
-      y: Math.random() * canvas.height,
-      radius: Math.random() * 1 + 4.5,
-      vx: Math.floor(Math.random() * 50) - 25,
-      vy: Math.floor(Math.random() * 50) - 25,
-    });
-  }
-
-  window.addEventListener("resize", resize);
-  function getNumOfStars() {
-    const numOfStars = window.innerWidth <= 576 ? 47 : 100;
-    return numOfStars;
-  }
-  function resize() {
-    x = getNumOfStars();
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-  }
-
-  // Draw the scene
-  function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx.globalCompositeOperation = "lighter";
-
-    for (var i = 0, x = stars.length; i < x; i++) {
-      var s = stars[i];
-
-      ctx.fillStyle = "rgba(0, 0, 0, 0.035)";
-      ctx.beginPath();
-      ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-      ctx.fill();
-      // ctx.fillStyle = "#fafafa";
-      // ctx.stroke();
-    }
-
-    ctx.beginPath();
-    for (var i = 0, x = stars.length; i < x; i++) {
-      var starI = stars[i];
-      ctx.moveTo(starI.x, starI.y);
-      if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-      for (var j = 0, x = stars.length; j < x; j++) {
-        var starII = stars[j];
-        if (distance(starI, starII) < 150) {
-          ctx.lineTo(starII.x, starII.y);
-        }
-      }
-    }
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "rgba(0, 0, 0, 0.03)";
-    ctx.stroke();
-  }
-
-  function distance(point1: Point, point2: Point) {
-    var xs = 0;
-    var ys = 0;
-
-    xs = point2.x - point1.x;
-    xs = xs * xs;
-
-    ys = point2.y - point1.y;
-    ys = ys * ys;
-
-    return Math.sqrt(xs + ys);
-  }
-
-  // Update star locations
-  function update() {
-    for (var i = 0, x = stars.length; i < x; i++) {
-      var s = stars[i];
-
-      s.x += s.vx / FPS;
-      s.y += s.vy / FPS;
-
-      if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-      if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-    }
-  }
-
-  canvas.addEventListener("mousemove", function (e) {
-    mouse.x = e.clientX;
-    mouse.y = e.clientY;
-  });
-
-  // Update and draw
-  function tick() {
-    draw();
-    update();
-    requestAnimationFrame(tick);
-  }
-
-  tick();
 }
 
 export { animations };
---
+++ scripts/animations/networkBackgroundAnimation.ts
@@ -0,0 +1,185 @@
+interface Star {
+  x: number;
+  y: number;
+  radius: number;
+  vx: number;
+  vy: number;
+}
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+const mouse = {
+  x: 0,
+  y: 0,
+}; // mouse location
+
+class NetworkBackgroundAnimation {
+  private options: Options;
+
+  constructor(private elems: Element[]) {
+    this.options = new Options();
+  }
+
+  public init(): void {
+    for (const elem of this.elems) {
+      const canvas: HTMLCanvasElement = this.getBgCanvas();
+      this.setParentElemPositionForCanvasToAlignWith(elem as HTMLElement);
+      const animator: Animator = new Animator(elem, canvas, this.options);
+      animator.init();
+      elem.append(canvas);
+    }
+  }
+
+  private getBgCanvas(): HTMLCanvasElement {
+    const canvas: HTMLCanvasElement = document.createElement("canvas");
+    canvas.style.position = "absolute";
+    canvas.style.top = "0";
+    canvas.style.left = "0";
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.style.zIndex = "-1";
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+    canvas.classList.add("trans-entrance-fade-in", "trans-d-1500");
+    return canvas;
+  }
+
+  private setParentElemPositionForCanvasToAlignWith(elem: HTMLElement): void {
+    if (elem.style.position.length === 0) {
+      elem.style.position = "relative";
+    }
+  }
+}
+
+class Animator {
+  private ctx: CanvasRenderingContext2D | null;
+  private stars: Star[]; // Array that contains the stars
+
+  constructor(
+    private elem: Element,
+    private canvas: HTMLCanvasElement,
+    private options: Options
+  ) {
+    this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.stars = [];
+  }
+
+  public init() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+
+    // Push stars to array
+    for (var i = 0; i < this.options.numOfStars; i++) {
+      this.stars.push({
+        x: Math.random() * this.canvas.width,
+        y: Math.random() * this.canvas.height,
+        radius: Math.random() * 1 + 4.5,
+        vx: Math.floor(Math.random() * 50) - 25,
+        vy: Math.floor(Math.random() * 50) - 25,
+      });
+    }
+
+    window.addEventListener(
+      "resize",
+      this.updateCanvasOnResize.bind(this, this.canvas)
+    );
+    this.canvas.addEventListener("mousemove", (e) => {
+      mouse.x = e.clientX;
+      mouse.y = e.clientY;
+    });
+
+    this.tick();
+  }
+
+  private updateCanvasOnResize(canvas: HTMLCanvasElement) {
+    this.options.numOfStars = getNumOfStars();
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+  }
+
+  // Update star locations
+  private update() {
+    for (var i = 0, x = this.stars.length; i < x; i++) {
+      const s = this.stars[i];
+      s.x += s.vx / this.options.fps;
+      s.y += s.vy / this.options.fps;
+      if (s.x < 0 || s.x > this.canvas.width) {
+        s.vx = -s.vx;
+      }
+      if (s.y < 0 || s.y > this.canvas.height) {
+        s.vy = -s.vy;
+      }
+    }
+  }
+
+  // Draw the scene
+  private draw(): void {
+    this.ctx!.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    //ctx.globalCompositeOperation = "lighter";
+
+    for (var i = 0, x = this.stars.length; i < x; i++) {
+      var s = this.stars[i];
+      this.ctx!.fillStyle = "rgba(0, 0, 0, 0.035)";
+      this.ctx!.beginPath();
+      this.ctx!.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+      this.ctx!.fill();
+      // ctx.fillStyle = "#fafafa";
+      // ctx.stroke();
+    }
+
+    this.ctx!.beginPath();
+    for (let i = 0, x = this.stars.length; i < x; i++) {
+      const starI = this.stars[i];
+      this.ctx!.moveTo(starI.x, starI.y);
+      if (this.distance(mouse, starI) < 150) this.ctx!.lineTo(mouse.x, mouse.y);
+      for (let j = 0, x = this.stars.length; j < x; j++) {
+        var starII = this.stars[j];
+        if (this.distance(starI, starII) < 150) {
+          this.ctx!.lineTo(starII.x, starII.y);
+        }
+      }
+    }
+    this.ctx!.lineWidth = 1;
+    this.ctx!.strokeStyle = "rgba(0, 0, 0, 0.03)";
+    this.ctx!.stroke();
+  }
+
+  private distance(point1: Point, point2: Point) {
+    var xs = 0;
+    var ys = 0;
+
+    xs = point2.x - point1.x;
+    xs = xs * xs;
+
+    ys = point2.y - point1.y;
+    ys = ys * ys;
+
+    return Math.sqrt(xs + ys);
+  }
+
+  private tick(): void {
+    this.draw();
+    this.update();
+    requestAnimationFrame(this.tick.bind(this));
+  }
+}
+
+class Options {
+  public fps: number; // Frames per second
+  public numOfStars: number; // Number of stars
+
+  public constructor() {
+    this.fps = 60;
+    this.numOfStars = getNumOfStars();
+  }
+}
+
+function getNumOfStars(): number {
+  const numOfStars = window.innerWidth <= 576 ? 47 : 100;
+  return numOfStars;
+}
+
+export { NetworkBackgroundAnimation };
